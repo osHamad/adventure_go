b9d46f289af22c0b4c1c28de38b0f76c0fad5d5b
osamaHamad0 <>
2021-05-11 21:43:10 +0000

    add sfx

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function linked to button which mutes and unmutes the music
-document.getElementById('mute-button').addEventListener('click', function(){
+document.getElementById('mute-button').addEventListener('click', function () {
   const music = document.getElementById('music')
   const button = document.getElementById('mute-button')
   // checks if music is paused and plays the music
@@ -17,3 +17,19 @@ document.getElementById('mute-button').addEventListener('click', function(){
 document.getElementById('play-button').addEventListener('click', function(){
   window.location.href = '/level-selector.html'
 })
+
+// make sound when hovering over menu page buttons
+document.getElementById('play-button').addEventListener('mouseover', hoverSound)
+document.getElementById('settings-button').addEventListener('mouseover', hoverSound)
+document.getElementById('code-button').addEventListener('mouseover', hoverSound)
+
+
+function hoverSound () {
+  const sound = document.getElementById('hover-sound')
+  sound.play()
+}
+
+function clcikSound () {
+  const sound = document.getElementById('click-sound')
+  sound.play()
+}
