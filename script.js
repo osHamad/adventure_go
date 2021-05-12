dfa65cc5f074f641d64b00837eb3eeee2b8ffff9
osamaHamad0 <>
2021-05-12 18:39:14 +0000

    visual novel added

--- script.js
+++ script.js
@@ -33,3 +33,48 @@ function clcikSound () {
   const sound = document.getElementById('click-sound')
   sound.play()
 }
+
+// this is the variable to know what to display on screen
+let visualCounter = 0
+
+// the script of what is said in the visual novel
+visualScript = [
+  'Kazuma, Kazuma!! All of our money somehow disappeared!!',
+  'Hmm, money? Oh that..',
+  'I spent it all yesterday at the tavern.',
+  'Megumin also came with me.',
+  'Kazuma..',
+  'Oi, oi, I just said Megumin also came with me!!!',
+  'Ok fine, I\'ll go earn back what I spent.. Sigh'
+
+]
+
+// order of images for the visual novel
+const visualImages = {
+  0: 'visual-novel/first.png',
+  1: 'visual-novel/second.png',
+  2: 'visual-novel/second.png',
+  3: 'visual-novel/third.png',
+  4: 'visual-novel/fourth.png',
+  5: 'visual-novel/fourth.png',
+  6: 'visual-novel/fifth.png',
+  7: 'visual-novel/fifth.png'
+
+}
+
+document.getElementById('text-box-next').addEventListener('click', nextFrame)
+function nextFrame (){
+  if (visualCounter < visualScript.length) {
+    let text = document.getElementById('text-box-text')
+    text.innerText = visualScript[visualCounter]
+    console.log(visualCounter)
+    document.getElementById('image-1').src = visualImages[visualCounter]
+    visualCounter ++
+    
+  } else {
+    document.getElementById('visual-novel').style.display = 'none'
+    document.getElementById('start-menu').style.display = 'block'
+  }
+  return 0
+}
+
